08119c406bb53857f643c575aa6d80fbeb7ba43d
Anay Goyal <anay.goyal@gmail.com>
2026-02-22 13:53:54 -0800

    Refactor SwapToWin: Replace original logic with new input handling for words array

--- SwapToWin.java
+++ SwapToWin.java
@@ -1,78 +1,22 @@
-import java.util.Arrays;
 import java.util.Scanner;
+import java.util.Arrays;
 public class SwapToWin {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int T = in.nextInt();
-        for(int ti = 0; ti < T; ti++) {
-            int N = in.nextInt();
-            int M = in.nextInt();
-            in.nextLine(); // consume newline
-            String t = in.nextLine().trim();
-            char[][] s = new char[N][M];
-            for(int i = 0; i < N; i++) {
-                String line = in.nextLine().trim();
-                while(line.length() < M) {
-                    String extra = in.nextLine();
-                    if(extra == null) break;
-                    line = extra.trim();
-                }
-                s[i] = line.toCharArray();
+        int repeat = in.nextInt();
+        for(int i=0;i<repeat;i++)
+        {
+            int wordsNumber = in.nextInt();
+            int letters = in.nextInt();
+            in.nextLine();
+            String[] wordsArray = new String[wordsNumber + 1];
+            for(int a = 0; a <= wordsNumber; a++)
+            {
+                wordsArray[a] = in.nextLine();
             }
-
-            java.util.List<String> ops = new java.util.ArrayList<>();
-
-            for(int pos = 0; pos < M; pos++) {
-                if(s[0][pos] == t.charAt(pos)) continue;
-                boolean done = false;
-                for(int j = pos+1; j < M && !done; j++) {
-                    if(s[0][j] == t.charAt(pos)) {
-                        swapInString(s[0], pos, j);
-                        ops.add(String.format("1 1 %d %d", pos+1, j+1));
-                        done = true;
-                    }
-                }
-                if(done) continue;
-                for(int k = 1; k < N && !done; k++) {
-                    if(s[k][pos] == t.charAt(pos)) {
-                        char tmp = s[0][pos];
-                        s[0][pos] = s[k][pos];
-                        s[k][pos] = tmp;
-                        ops.add(String.format("2 1 %d %d", k+1, pos+1));
-                        done = true;
-                    }
-                }
-                if(done) continue;
-                for(int k = 1; k < N && !done; k++) {
-                    for(int j = 0; j < M && !done; j++) {
-                        if(s[k][j] == t.charAt(pos)) {
-                            if(j != pos) {
-                                swapInString(s[k], j, pos);
-                                ops.add(String.format("1 %d %d %d", k+1, j+1, pos+1));
-                            }
-                            char tmp = s[0][pos];
-                            s[0][pos] = s[k][pos];
-                            s[k][pos] = tmp;
-                            ops.add(String.format("2 1 %d %d", k+1, pos+1));
-                            done = true;
-                        }
-                    }
-                }
-            }
-
-            if(ops.size() > 2 * M) {
-                System.err.println("WARNING: ops exceed 2M: " + ops.size());
-            }
-            System.out.println(ops.size());
-            for(String op : ops) {
-                System.out.println(op);
-            }
+            System.out.println(Arrays.toString(wordsArray));
         }
-    }
+        
 
-    private static void swapInString(char[] arr, int i, int j) {
-        char tmp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = tmp;
     }
 }
